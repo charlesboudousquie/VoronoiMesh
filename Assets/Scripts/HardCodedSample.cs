8e7fe929d43414368378c28b459bf35d8c3c0541
Trevor Harron <trevor.harron@digipen.edu>
2019-07-04 15:51:27 -0700

    moved files ot Assest/Scripts, adding mroe functionality to the navemesh3 and node

---
+++ Assets/Scripts/HardCodedSample.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HardCodedSample : MonoBehaviour
+{
+    public VoronoiNode rootNode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this is just for the test version, in the actual model we should just load the mesh in with the normal unity componenet
+        MeshFilter mf = GetComponent<MeshFilter> ();
+        Mesh mesh = new Mesh();
+        mf.mesh = mesh;
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(0, 0, 0);
+        vertices[1] = new Vector3(1, 0, 0);
+        vertices[2] = new Vector3(0, 0, 1);
+        vertices[3] = new Vector3(0, 1, 0);
+        mesh.vertices = vertices;
+
+        //add the points to the tris in groups of 3 in(ccw?) rotational order, 
+        //if the normal is wrong change the order
+        int[] tris = new int[] {
+            0, 1, 2,
+            1, 0, 3,
+            0, 2, 3,
+            2, 1, 3
+        };
+        mesh.triangles = tris;
+
+        //normals are by vertex and a pain in the ass to figure out, just use default
+        mesh.RecalculateNormals();
+
+
+        rootNode = new VoronoiNode(0);
+        VoronoiNode n1 = new VoronoiNode(1);
+        VoronoiNode n2 = new VoronoiNode(2);
+        VoronoiNode n3 = new VoronoiNode(3);
+
+        rootNode.Start();
+        n1.Start();
+        n2.Start();
+        n3.Start();
+
+        rootNode.AddPoint(vertices[0]);
+        rootNode.AddPoint(vertices[1]);
+        rootNode.AddPoint(vertices[2]);
+        rootNode.AddNeighbor(ref n1);
+        rootNode.AddNeighbor(ref n2);
+        rootNode.AddNeighbor(ref n3);
+
+        n1.AddPoint(vertices[1]);
+        n1.AddPoint(vertices[0]);
+        n1.AddPoint(vertices[3]);
+        n1.AddNeighbor(ref rootNode);
+        n1.AddNeighbor(ref n2);
+        n1.AddNeighbor(ref n3);
+
+        n2.AddPoint(vertices[0]);
+        n2.AddPoint(vertices[2]);
+        n2.AddPoint(vertices[3]);
+        n2.AddNeighbor(ref n1);
+        n2.AddNeighbor(ref rootNode);
+        n2.AddNeighbor(ref n3);
+
+        n3.AddPoint(vertices[2]);
+        n3.AddPoint(vertices[1]);
+        n3.AddPoint(vertices[3]);
+        n3.AddNeighbor(ref n1);
+        n3.AddNeighbor(ref n2);
+        n3.AddNeighbor(ref rootNode);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/VoronoiNode.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoronoiNode
+{
+
+    private int[] neighbors;
+    private Vector3[] vertices;
+    private Vector3 normal;
+    public Vector3 Position { get; set; }
+    int numVerts, numNeighbors;
+    public int Id { get; }
+    public float Cost;
+    public float Hueristic;//recommend Euclidean distance
+    public float Given;
+    public bool Open;
+    public bool Closed;
+    public int Parent;
+    public VoronoiNode(int _id)
+    {
+        Id = _id;
+    }
+    // Start is called before the first frame update
+    public void Start()
+    {
+        Open = Closed = false;
+        Cost = Hueristic = Given = 0;
+        neighbors = new int[3];
+        vertices = new Vector3[3];
+        numVerts = 0;
+        numNeighbors = 0;
+    }
+
+    public void Reset()
+    {
+        Open = Closed = false;
+        Cost = Hueristic = Given = 0;
+    }
+
+    public int[] GetNeighbors() {
+        return neighbors;
+    }
+
+    public Vector3[] GetVertices()
+    {
+        return vertices;
+    }
+
+    public void AddNeighbor(VoronoiNode n) {
+        if (numNeighbors < 3) {
+            neighbors[numNeighbors] = n.Id;
+            numNeighbors++;
+        } else {
+            Debug.Log("Attempt to add 4th neighbor.");
+        }
+    }
+
+    public void SetNormal(Vector3 n) {
+        normal = n;
+    }
+
+
+    public void AddPoint(Vector3 p) {
+        if (numVerts < 3) {
+            vertices[numVerts] = p;
+            numVerts++;
+        } else {
+            Debug.Log("Attempt to add 4th vert.");
+        }
+        if(numVerts == 3)
+        {
+            Position = (vertices[0] + vertices[1] + vertices[2]) / 3.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
