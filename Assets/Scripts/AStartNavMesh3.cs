f8e2872ed44bcb49c0e7f1330914cd43948a92ae
nick <nvmsocool@yahoo.com>
2019-07-04 15:29:07 -0700

    some changes, and version fix
    ?

---
+++ Assets/Scripts/AStartNavMesh3.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStartNavMesh3 : MonoBehaviour
+{
+    NodeHeap openHeap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VoronoiNode[] list = new VoronoiNode[1];
+        openHeap.InitHeap(list);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public List<Vector3> GetPath(VoronoiNode start, VoronoiNode end) {
+        List<Vector3> path = new List<Vector3>();
+
+        int numNodes = 0;
+        for (int i = 0; i < numNodes; i++) {
+            //reset all nodes somehow
+        }
+        
+        openHeap.ResetHeap();
+
+        //int s = start.Id;
+        int s = 0;
+        openHeap.Push(s);
+        //start.Open = true;
+
+        /*
+        while (!openHeap.IsEmpty()) {
+
+            VoronoiNode least = openHeap.Pop();
+
+            lx = least->x();
+            ly = least->y();
+
+            if (least.Id == end.Id) {
+                //celebrate: push stuff onto solution path and return success
+                while (least.Id != start.Id) {
+                    path.Add(least.position);
+                    least = least.parent;
+                }
+                path.Add(start.position);
+                return path;
+            }
+
+            least->SetClosed();
+            if (request.settings.debugColoring) terrain->set_color(ly, lx, Colors::Blue);
+            nbrList = least->GetNeighbors();
+
+            for (int i = 0; i < 8; i++) {
+                if (!(nbrList & (0b00000001 << i))) continue;
+                nbr = getNodeFromInt(i, lx, ly);
+                if (nbr->OnOpen()) {
+                    if (nbr->GetGivenS() - (least->GetGivenS() + costS[i]) > (least->GetGivenD() + costD[i] - nbr->GetGivenD()) * 1.141) {
+                        nbr->Open(least, costS[i], costD[i]);
+                        openHeap.UpdateNode(nbr->x(), nbr->y());
+                    }
+                } else if (!nbr->OnClosed()) {
+                    nx = nbr->x();
+                    ny = nbr->y();
+                    nbr->Open(least, costS[i], costD[i]);
+                    SetHeuristic(nbr, std::abs(goal.col - nx), std::abs(goal.row - ny), request.settings.heuristic);
+                    openHeap.Push(nbr);
+                    if (request.settings.debugColoring) terrain->set_color(ny, nx, Colors::Yellow);
+                }
+            }
+            if (request.settings.singleStep) return PathResult::PROCESSING;
+        }
+        //handle error: no path exists
+        return PathResult::IMPOSSIBLE;
+        */
+
+
+        return path;
+    }
+}
---
+++ Assets/Scripts/NodeHeap.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeHeap
+{
+    int[] locationMap;
+    VoronoiNode[] nodeHeap;
+    VoronoiNode nodeStart;
+    int numNodes, m, f, l, r, p;
+
+    public void Start() {
+        locationMap = new int[4096];
+        nodeHeap = new VoronoiNode[4096];
+    }
+
+    public void InitHeap(VoronoiNode[] l) {
+        nodeStart = l[0];
+        for (int i = 0; i < 4096; i++) {
+            locationMap[i] = i;
+            nodeHeap[i] = l[i];
+        }
+    }
+
+    public bool IsEmpty() {
+        return numNodes == 0;
+    }
+
+    public void Push(int i) {
+        SwapNodes(locationMap[i], numNodes);
+        numNodes++;
+        TrickleUp(numNodes - 1);
+    }
+
+    public void UpdateNode(int i) {
+        TrickleUp(locationMap[i]);
+    }
+
+    public VoronoiNode Pop() {
+        //could test for size 0 here, but wont for speed
+        VoronoiNode top = nodeHeap[0];
+        numNodes--;
+        SwapNodes(0, numNodes);
+        TrickleDown(0);
+        return top;
+    }
+
+    public void ResetHeap() {
+        numNodes = 0;
+    }
+
+    void SwapNodes(int i, int j) {
+        VoronoiNode tNode = nodeHeap[i];
+        nodeHeap[i] = nodeHeap[j];
+        nodeHeap[j] = tNode;
+        //locationMap[nodeHeap[j].Id] = j;
+        //locationMap[nodeHeap[i].Id] = i;
+    }
+
+    void TrickleDown(int i) {
+        SetTD(i);
+        while (NodeLT(nodeHeap[m], nodeHeap[f])) {
+            SwapNodes(f, m);
+            SetTD(m);
+        }
+    }
+
+    void SetTD(int i) {
+        f = i;
+        l = (i << 1) + 1;
+        r = l + 1;
+        if (r < numNodes) {
+            m = NodeLT(nodeHeap[l], nodeHeap[r]) ? l : r;
+        } else if (l >= numNodes) {
+            m = f;
+        } else {
+            m = l;
+        }
+    }
+
+    void TrickleUp(int i) {
+        SetTU(i);
+        if (f == 0) return;
+        while (NodeLT(nodeHeap[f], nodeHeap[p])) {
+            SwapNodes(f, p);
+            SetTU(p);
+            if (f == 0) return;
+        }
+    }
+
+    void SetTU(int i) {
+        f = i;
+        p = (i - 1) >> 1;
+    }
+
+    bool NodeLT(VoronoiNode a, VoronoiNode b) {
+        return false;
+        //return (a.Cost < b.Cost);
+    }
+
+}
